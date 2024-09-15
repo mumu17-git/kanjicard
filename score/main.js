0d6309a77b4cbdff29b984a02d470d72656189d7
mumu17-git <moonpapa1709@gmail.com>
2024-09-15 15:40:59 +0900

    update

--- score/main.js
+++ score/main.js
@@ -1,18 +1,47 @@
 import {app} from '../firebaseApp.js'
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js"
 
+const di =(id)=>{return document.getElementById(id)}
 var nowgameid = null;
-var playerData = [];
+var playerData = null;
 var gameData = null;
+var first = true;
+
+function reloadPoint(pd) {
+    
+    for(var i = 0;i < pd.length;i++) {
+        di("player"+String(i+1)+"_name").textContent = gameData["nameList"][i];
+        di("player"+String(i+1)+"_point").textContent = String(pd[i]["NumberOfGotCards"]);
+        if(pd[i]["turn"] == gameData["turn"]) di("playerTurn").textContent = "　　　　　"+gameData["nameList"][i]+"のターンです。"
+    }
+
+    
+    setTimeout(db_loadInit,6000);
+}
+
+function initScreen(i) {
+    var num1 = Math.floor((i)/3)+1;
+    var num2 = (i) % 3;
+    if(num2 == 0) num2 = 3;
+
+    di("bm-td"+String(num1)+"-"+String(num2)).insertAdjacentHTML("beforeend",'<div class="box-010"><span id="player'+String(i+1)+'_name"></span><p>取ったカードの枚数　　<strong id="player'+String(i+1)+'_point"></strong></p></div>')
+}
 
 async function db_load() {
     var db = getFirestore(app);
-    console.log(gameData);
+    playerData = [];
     for(var i = 0;i < gameData["maxplayer"];i++) {
-        const userRef = doc(db, nowgameid, "player"+i);
+        const userRef = doc(db, nowgameid, gameData["nameList"][i]);
         const docSnap = await getDoc(userRef);
-        playerData.push(docSnap.data());
+        if (docSnap.exists()) {
+            playerData.push(docSnap.data());
+            if(first) {
+                initScreen(i);
+            }
+            if(i == gameData["maxplayer"]-1) reloadPoint(playerData);
+        }
     }
+    first = false;
 }
 
 async function db_loadInit() {
@@ -34,6 +63,8 @@ async function db_loadInit() {
 function setup() {
     db_loadInit();
 }
+
+
 
 
 setup();
